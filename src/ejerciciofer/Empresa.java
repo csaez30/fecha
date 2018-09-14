5c5e3f2a3b40284f0412cb96ff715722207e400e
babolat22 <jjsaez@ulp.edu.ar>
2018-09-14 19:01:10 -0300

    Version 1.0

--- src/ejerciciofer/Empresa.java
+++ src/ejerciciofer/Empresa.java
@@ -10,5 +10,40 @@ package ejerciciofer;
  * @author juanj
  */
 public class Empresa {
+      String razonSocial;
+      int CUIT;
+      Empleado []empleados;
+
+    public Empresa() {
+    }
       
+    public Empresa(String razonSocial, int CUIT, Empleado[] empleados) {
+        this.razonSocial = razonSocial;
+        this.CUIT = CUIT;
+        this.empleados = empleados;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public int getCUIT() {
+        return CUIT;
+    }
+
+    public void setCUIT(int CUIT) {
+        this.CUIT = CUIT;
+    }
+
+    public Empleado[] getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(Empleado[] empleados) {
+        this.empleados = empleados;
+    }
 }
