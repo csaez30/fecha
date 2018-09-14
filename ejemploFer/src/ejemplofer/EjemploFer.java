33680a50c3b2d67e6524a5767dcf24c6298c5ad0
csaez30 <csaez2007@gmail.com>
2018-09-14 19:01:38 -0300

    clase empleado, por cris

---
+++ ejemploFer/src/ejemplofer/EjemploFer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplofer;
+
+/**
+ *
+ * @author cris
+ */
+public class EjemploFer {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
